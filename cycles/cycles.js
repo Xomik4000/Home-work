a44400f1dfc56fe081bab13c0aba922374ebaff6
Alexandr Bokov <pd.sk@bk.ru>
2025-05-16 14:06:46 +0300

    cycles

---
+++ cycles/cycles.js
@@ -0,0 +1,12 @@
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+} //первое задание
+
+let sum = 0
+let i = 1
+while (i <= 100) {
+    sum += i
+    i++
+}
+console.log(`Сумма чисел от 1 до 100 равна ${sum}`) //Второе задание
+
