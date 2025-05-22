8dfa4d05b8158a654454b0226317e2c8f5caee10
Alexandr Bokov <pd.sk@bk.ru>
2025-05-22 14:01:43 +0300

    primitives

---
+++ primitives/primitives.js
@@ -0,0 +1,24 @@
+const str1 = 'JavaScript is fun!'
+console.log(str1.includes('fun')) //первое задание
+
+
+let num1
+
+if (num1 === undefined) {
+   console.log(num1)
+} // Второе задание
+
+const firstName = 'John'
+const lastName = 'Doe'
+const occupation = 'software developer.'
+const result = `Hello, mu name is ${firstName + ' ' + lastName}. I am a ${occupation}`
+console.log(result) //Третье задание
+
+let a
+const b = null
+console.log(a === b)
+console.log(a == b) //Четвёртое задание
+
+const n1 = 1
+const n2 = '1'
+console.log(n1 + n2)// Пятое задание
