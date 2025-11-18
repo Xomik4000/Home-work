7ea27204e65713fd9ad25121c58f2ca8205a375c
Alexandr Bokov <pd.sk@bk.ru>
2025-11-18 11:15:43 +0300

    dz correction

--- setTimeOutAndSetInerval/js.js
+++ setTimeOutAndSetInerval/js.js
@@ -1,20 +1,45 @@
-let count = 10 
+// let count = 10 
 
-function showTimer(prefix) {
-  console.log(`${prefix}: ${count}`);
-  count--
-  const timer = setTimeout(showTimer, 1000, 'Осталось секунд')
-  if (count < 0) {
-    clearTimeout(timer)
+// function showTimer(prefix) {
+//   console.log(`${prefix}: ${count}`);
+//   count--
+//   const timer = setTimeout(showTimer, 1000, 'Осталось секунд')
+//   if (count < 0) {
+//     clearTimeout(timer)
+//   }
+// }
+
+// setTimeout(showTimer, 1000, 'Осталось секунд')
+// //Первое задание
+
+// function showMessageDrinkWater(message) {
+//   console.log(message);
+// }
+
+// setInterval(showMessageDrinkWater, 1800000, 'Не забудь выпить воды!')
+// //Второе задание
+
+const inputDelay = document.getElementById('delay');
+const inputText = document.getElementById('text');
+const myButton = document.getElementById('myButton')
+
+function handleButton() {
+  console.log('Задержка:',inputDelay.value);
+  console.log('Текст:', inputText.value);
+}
+
+let intervalId = null
+
+myButton.addEventListener('click', (e) => {
+  e.preventDefault()
+  if (intervalId !== null) {
+    clearInterval(intervalId);
+    intervalId = null;
+    console.log('текст не выводится');
+    return
   }
-}
+  
+  intervalId = setInterval(handleButton, 2000)
+  console.log('текст запущен с задержкой');
+})
 
-setTimeout(showTimer, 1000, 'Осталось секунд')
-//Первое задание
-
-function showMessageDrinkWater(message) {
-  console.log(message);
-}
-
-setInterval(showMessageDrinkWater, 1800000, 'Не забудь выпить воды!')
-//Второе задание
