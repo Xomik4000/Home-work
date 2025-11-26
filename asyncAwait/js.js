be92d3208749763c5be1e4da6199b613f1f24f4e
Alexandr Bokov <pd.sk@bk.ru>
2025-11-26 10:19:44 +0300

    dz correct  async/await

--- asyncAwait/js.js
+++ asyncAwait/js.js
@@ -3,19 +3,26 @@
 
  async function delay(ms) {
     const data = await new Promise(resolve => setTimeout(resolve, ms, 'Задержка завершена'))
-    console.log(data)
+    return data
 } 
 
-delay(2000) // первое задание
+delay(2000)
+    .then((message) => console.log('Сообщение получено:', message)) // первое задание
 
 async function fetcAsyncUsers(url) {
     try {
         const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
         const data = await response.json()
-        console.log(`Имя: ${data.name}, email: ${data.email}, адресс: ${data.address.street}. ${data.address.suite} `)
+        return data
     } catch (e) {
-        console.error(e)
+        console.error('Ошибка при получении данных:', e)
+        throw error
     }
 }
 
-fetcAsyncUsers('https://jsonplaceholder.typicode.com/users/1') // Второе задание
+fetcAsyncUsers('https://jsonplaceholder.typicode.com/users/1')
+    .then(user => console.log(`Имя: ${user.name}, email: ${user.email}, адресс: ${user.address.street}. ${user.address.suite} `))
+    .catch(error => console.error('Произошла ошибка:', error)) // Второе задание
