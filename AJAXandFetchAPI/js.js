b90b2b511364dce64129c99429ec6cdf0f43a691
Alexandr Bokov <pd.sk@bk.ru>
2025-12-09 12:57:50 +0300

    dz correct  AJAX and Fetch AP

--- AJAXandFetchAPI/js.js
+++ AJAXandFetchAPI/js.js
@@ -10,6 +10,10 @@ function sendRequest(method, url, body = null) {
         headers: headers
     })
         .then(response => {
+            if (method === 'DELETE') {
+                return true
+            }
+
             if (response.ok){
                 return response.json()
             } else {
@@ -25,32 +29,33 @@ function sendRequest(method, url, body = null) {
 
 
 
-const requestURL = 'https://jsonplaceholder.typicode.com/users'
+const postURL = 'https://jsonplaceholder.typicode.com/posts'
+const answer = document.getElementById('answer')
 
-document.getElementById('loadForm').addEventListener('click', (e) => {
+document.getElementById('loadForm').addEventListener('submit', (e) => {
     e.preventDefault()
+
     const inputs = document.querySelectorAll('input')
     let dataForms = {}
-    const allowedKeys = ['userId', 'header', 'text']
+    const allowedKeys = ['userId', 'title', 'body']
     inputs.forEach(input => {
         const key = input.name;
         if (allowedKeys.includes(key)) {
             dataForms[key] = input.value
         }
     })
-    sendRequest('POST', requestURL, dataForms)
+    sendRequest('POST', postURL, dataForms)
         .then(data => {
             console.log(data)
             for (const key in data) {
                 let postParagrapg = document.createElement('p')
                 postParagrapg.textContent = key + ': ' + data[key]
-                document.body.appendChild(postParagrapg)
+                answer.appendChild(postParagrapg)
             }
         })
         .catch(error => console.error('Ошибка:', error))
 }) //первое задание
 
-const postURL = 'https://jsonplaceholder.typicode.com/posts'
 
 document.getElementById('loadPost').addEventListener('click', (e) => {
     e.preventDefault()
@@ -62,10 +67,88 @@ document.getElementById('loadPost').addEventListener('click', (e) => {
             postList.innerHTML = ''
             posts.forEach(post => {
                 const listItem = document.createElement('li')
+                listItem.id = post.id
                 listItem.textContent = `ID: ${post.id} - ${post.title}`
+
+                const deleteButton = document.createElement('button')
+                deleteButton.textContent = 'удалить пост'
+                deleteButton.addEventListener('click', () => deletePost(post.id))
+
+                listItem.appendChild(deleteButton)
                 postList.appendChild(listItem)
             })
         })
         .catch(error => console.error('Ошибка:', error))
 }) //второе задание
 
+function deletePost(id) {
+    sendRequest('DELETE', `${postURL}/${id}`)
+        .then(() => {
+            console.log(`пост ${id} удалён`)
+
+            const postItem = document.getElementById(id)
+            if (postItem) {
+                postItem.remove()
+            }
+        })
+        .catch(error => console.error('Ошибка:', error))
+}
+//третье задание
+
+const userURL = 'https://jsonplaceholder.typicode.com/users'
+const updatedUser = document.getElementById('updatedUser')
+
+
+document.getElementById('loadFormUsers').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const inputs = e.target.elements
+    console.log(inputs)
+    let dataForms = {}
+    const allowedKeys = ['name', 'email']
+    allowedKeys.forEach(key => {
+        if (inputs[key].value.trim() !== '') {
+            dataForms[key] = inputs[key].value;
+        }
+    })
+    const userId = inputs.id.value 
+    sendRequest('PUT', `${userURL}/${userId}`, dataForms)
+        .then(data => {
+            console.log('Пользователь обновлён:', data)
+            updatedUser.innerHTML = '';
+            for (const key in data) {
+                let postParagrapg = document.createElement('p')
+                postParagrapg.textContent = key + ': ' + data[key]
+                updatedUser.appendChild(postParagrapg)
+            }
+            const userItem = document.getElementById(`user-${data.id}`)
+            if (userItem) {
+                userItem.textContent = `ID: ${data.id} - ${data.name} - ${data.email}`
+            }
+        })
+        .catch(error => console.error('Ошибка:', error))
+}) // PUT запрос
+
+const patchedUser = document.getElementById('patchedUser')
+
+document.getElementById('patchUserForm').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const inputs = e.target.elements;
+    const userData = {
+        email: inputs.email.value
+    }
+    const userId = inputs.id.value
+    fetch(`${userURL}/${userId}`, {
+        method: 'PATCH',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify(userData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            patchedUser.innerHTML = `ID: ${data.id}, Email: ${data.email}`
+        })
+        .catch(error => console.error('Ошибка:', error))
+}) //PUTCH запрос
+//четвёртое задание
