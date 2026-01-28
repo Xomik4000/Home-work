b07152798053a0006acb45c1a1f12c340e72f501
Alexandr Bokov <pd.sk@bk.ru>
2026-01-28 14:04:11 +0300

    dz hooks

---
+++ hooks/src/App.jsx
@@ -0,0 +1,58 @@
+import { use, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Counter() {
+  const [count, setCount] = useState(10)
+
+  const decrement = () => {
+    setCount(prevState => Math.max(prevState - 1, 0))
+  }
+
+  return (
+    <div>
+      <p>Текущее значение: {count}</p>
+      <button onClick={decrement}>Уменьшить</button>
+      
+      {count === 0 && (
+        <p style={{ color: 'red' }}>Пожалуйста, измените количество, оно не может быть равно 0</p>
+      )}
+    </div>
+  )
+}
+
+
+
+function App() {
+  const [postId, setPostId] = useState('');
+  const [post, setPost] = useState(null);
+
+  const getPost = () => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+      .then(res => res.json())
+      .then(data => setPost(data))
+  }
+
+  return (
+    <>
+      <Counter />
+      <input 
+        placeholder='ID поста'
+        value={postId} 
+        onChange={e => setPostId(e.target.value)} 
+      />
+
+      <button onClick={getPost}>Получить пост</button>
+
+      {post && (
+        <div>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default App
