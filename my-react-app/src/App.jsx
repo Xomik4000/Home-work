feb95d105c5bc9ccf6efd868c3b737e0af3effa2
Alexandr Bokov <pd.sk@bk.ru>
2026-02-20 15:02:45 +0300

    useEffect Vladillen

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,20 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const tasks = ['Помыть посуду', 'Сделать домашнее задание', 'Прочитать книгу'];
+  const [type, setType] = useState("users");
+
+  const [data, setData] = useState([]);
+
+  const [pos, setPos] = useState({
+    x: 0,
+    y: 0,
+  });
+  // useEffect(() => {
+  //   console.log('рендер компонента')
+  // })
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
+      .then((response) => response.json())
+      .then((json) => setData(json));
+
+    return () => {
+      console.log('clean type')
+    }
+  }, [type]);
+
+  const mouseMoveHandler = (e) => {
+    setPos({
+      x: e.clientX,
+      y: e.clientY,
+    });
+  };
+
+  useEffect(() => {
+    console.log("ComponentDidMount");
+
+    window.addEventListener("mousemove", mouseMoveHandler);
+
+    return () => {
+       window.removeEventListener("mousemove", mouseMoveHandler);
+    }
+  }, []);
+
   return (
     <div>
-      <h1>Список дел</h1>
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>{task}</li>
-        ))}
-      </ul>
+      <h1>Ресурс: {type}</h1>
+
+      <button onClick={() => setType("users")}>Пользователи</button>
+      <button onClick={() => setType("todos")}>Todo</button>
+      <button onClick={() => setType("posts")}>Посты</button>
+
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+      <pre>{JSON.stringify(pos, null, 2)}</pre>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
+
+
+const tasks = [
+  { id: "a1", text: "Купить молоко" },
+  { id: "b2", text: "Почитать книгу" },
+  { id: "c3", text: "Сделать зарядку" },
+];
+
+function TodoList() {
+  return (
+    <ul>
+      {tasks.map((task) => (
+        <li key={task.id}>{task.text}</li> // ✅ Лучший вариант!
+      ))}
+    </ul>
+  );
+}
