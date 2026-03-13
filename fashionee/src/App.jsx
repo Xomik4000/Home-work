13537e41a6fec1e876300527c498065a0737c56b
Alexandr Bokov <pd.sk@bk.ru>
2026-03-13 14:33:31 +0300

    fashionee

--- fashionee/src/App.jsx
+++ fashionee/src/App.jsx
@@ -3,25 +3,14 @@ import Shop from "./pages/Shop";
 import Cart from "./pages/Cart";
 import { LS_KEYS, PAGES } from "./constants";
 import { PRODUCTS } from "./data/products";
+import useLocalStorage from "./hooks/useLocalStorage";
 
 function App() {
   const [page, setPage] = useState(PAGES.SHOP);
 
-  const [favorites, setFavorites] = useState(
-    JSON.parse(localStorage.getItem(LS_KEYS.FAVORITES)) || [],
-  );
+  const [favorites, setFavorites] = useLocalStorage(LS_KEYS.FAVORITES, []);
 
-  const [cart, setCart] = useState(
-    JSON.parse(localStorage.getItem(LS_KEYS.CART)) || {},
-  );
-
-  useEffect(() => {
-    localStorage.setItem(LS_KEYS.FAVORITES, JSON.stringify(favorites));
-  }, [favorites]);
-
-  useEffect(() => {
-    localStorage.setItem(LS_KEYS.CART, JSON.stringify(cart));
-  }, [cart]);
+  const [cart, setCart] = useLocalStorage(LS_KEYS.CART, {});
 
   const addToCart = (id) => {
     setCart((prev) => {
