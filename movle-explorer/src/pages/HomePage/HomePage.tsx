aa8ac207cfd149f5832748cae5ab5f7311d7f490
Alexandr Bokov <pd.sk@bk.ru>
2026-06-06 14:14:34 +0300

    Add HomePage

--- movle-explorer/src/pages/HomePage/HomePage.tsx
+++ movle-explorer/src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,52 @@
+import { useState } from 'react';
+import { searchMovies } from '../../features/movies/api/movieApi';
+import type { Movie } from '../../features/movies/types/movie.types';
+import { SearchBar } from '../../components/SearchBar/SearchBar';
+
 export function HomePage() {
-  return <h1>Search movies</h1>;
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleSearch = async () => {
+    if (!query.trim()) {
+      return;
+    }
+
+    try {
+      setIsLoading(true);
+      setError(null);
+
+      const data = await searchMovies(query);
+
+      setMovies(data.movies);
+    } catch {
+      setError('Failed to load movies');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <section>
+      <h1>Search movies</h1>
+
+      <SearchBar value={query} onChange={setQuery} onSubmit={handleSearch} />
+
+      {isLoading && <p>Loading...</p>}
+
+      {error && <p>{error}</p>}
+
+      {!isLoading && !error && movies.length > 0 && (
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie.id}>
+              {movie.title} — {movie.releaseDate || 'Unknown year'}
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
 }
